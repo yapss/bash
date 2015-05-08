5e7a86d7f3009eb86383a6c8b9feec623e1ff6df
Brett Krueger <brettisnotfunny@gmail.com>
2015-05-08 00:56:06 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -7,25 +7,25 @@
 ## Doesn't exist
 
 unset path;
-if ! [ -d "${1}" ]
+if ! [ -d "${1}" ] && ! [ -f "${1}"  ]
 then
         echo -e "Usage: backup /path/to/backup /path/to/save\nIf no save path is defined, default on ."
         exit 1
 else
         path=$1;
-	save=$2;
-	check1=$(grep -o $path /);
-	if ! [ -d "${2}" ]
-	then
-		save=".";
-	fi
+        save=$2;
+        check1=$(grep -o $path /);
+        if ! [ -d "${2}" ]
+        then
+                save=".";
+        fi
 fi
 
 # Backup the thing
 
 if $check1
 then
-	save="${save}/backup_$(date +%s).tar.gz";
-	back=$(tar czpf $save $path);
-	echo -e "Backup Completed\nmd5sum: $(md5sum "${save}")";
+        save="${save}/${USER}_backup_$(date +%s).tar.gz";
+        back=$(tar czpf $save $path);
+        echo -e "Backup Completed\nmd5sum: $(md5sum "${save}")";
 fi
